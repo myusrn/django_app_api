be2a826a69a43054a8f03d9c5a37292136dedd7a
myusrn <myusrn@outlook.com>
2022-08-22 22:18:57 -0700

    fixing bug in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#echo 'collect static files'
-#python manage.py collectstatic --noinput
+# echo 'collect static files'
+# python manage.py collectstatic --noinput
 
 echo 'apply database migrations'
 python manage.py migrate
 
-echo 'create superuser'
-python manage.py createsuperuser
+# echo 'create superuser'
+# python manage.py createsuperuser --username django --email django@domain.com --<missing switch for password> SomeHardToGuessPassphrase
 
 echo 'start server'
-#python manage.py runserver # (default) expose on all interfaces ??? using port 8000
+# python manage.py runserver # (default) expose on all interfaces ??? using port 8000
 python manage.py runserver 0.0.0.0:8000 # explose on all interfaces using port 8000
