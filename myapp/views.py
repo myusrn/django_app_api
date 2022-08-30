15b84789252628868324f6ee351f203a0538a894
myusrn <myusrn@outlook.com>
2022-08-29 19:17:08 -0700

    ignore and view updates

--- myapp/views.py
+++ myapp/views.py
@@ -20,7 +20,11 @@ def secrets(request):
         # in powershell set client secret using $env:ClientSecret = "<value>" and clear using $env:ClientSecret = ""
         # in bash set debug run passwork using export ClientSecret='<value>' and clear using unset ClientSecret
         # in drekar k8s container deployment use rancher or k8s secrets manager to expose this environment variable
-        import os; client_secret = os.environ.get('ClientSecret', 'ClientSecret not found in environment') # api with support for defining default value if not present
-        return HttpResponse(f"<div style='font-family: verdana, serif, \
-            sans-serif; font-size: medium'>ClientSecret = {client_secret}</div>")
+        import os; 
+        client_secret = os.environ.get('ClientSecret', 'ClientSecret not found in environment') # api with support for defining default value if not present
+        secret_key = os.environ.get('SECRET_KEY', 'SECRET_KEY not found in environment')
+        return HttpResponse(f"<div style='font-family: verdana, serif, sans-serif; font-size: medium'> \
+                ClientSecret = {client_secret}<br> \
+                SECRET_KEY = {secret_key}<br> \
+                </div>")
 
